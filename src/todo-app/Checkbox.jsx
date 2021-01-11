a4a200beb16bbfa226d7443b06f5c1b40988c45d
hikariumeno <hikariumeno@gmail.com>
2021-01-11 19:59:02 +0900

    fix checkbox status

--- src/todo-app/Checkbox.jsx
+++ src/todo-app/Checkbox.jsx
@@ -6,13 +6,12 @@ export class Checkbox extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      checked: Boolean,
+      checked: false,
     };
     this.handleToggle = this.handleToggle.bind(this);
   }
 
   handleToggle(event) {
-    console.log(this.state.checked);
     this.setState({ checked: event.target.checked });
   }
 
@@ -21,12 +20,8 @@ export class Checkbox extends React.Component {
     const delLabel = <del>{this.props.label}</del>;
 
     return (
-      <Form.Check
-        type="checkbox"
-        checked={this.state.checked}
-        onChange={this.handleToggle}
-      >
-        <Form.Check.Input type="checkbox" />
+      <Form.Check type="checkbox" checked={this.state.checked}>
+        <Form.Check.Input type="checkbox" onChange={this.handleToggle} />
         <Form.Check.Label>
           {this.state.checked ? delLabel : label}
         </Form.Check.Label>
