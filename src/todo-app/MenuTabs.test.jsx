65963db48414a4d99de680579d4ac87016734887
hikariumeno <hikariumeno@gmail.com>
2021-03-16 00:16:06 +0900

    Add Coveralls to ci

--- src/todo-app/MenuTabs.test.jsx
+++ src/todo-app/MenuTabs.test.jsx
@@ -3,9 +3,27 @@ import renderer from "react-test-renderer";
 import { MenuTabs } from "./MenuTabs";
 
 test("All Tab selected", () => {
-  const onSelect = jest.fn()
+  const onSelect = jest.fn();
   const tree = renderer.create(
     <MenuTabs activeKey="All" onSelect={onSelect}></MenuTabs>
+  );
+  expect(onSelect).not.toHaveBeenCalled();
+  expect(tree.toJSON()).toMatchSnapshot();
+});
+
+test("Active Tab selected", () => {
+  const onSelect = jest.fn();
+  const tree = renderer.create(
+    <MenuTabs activeKey="Active" onSelect={onSelect}></MenuTabs>
+  );
+  expect(onSelect).not.toHaveBeenCalled();
+  expect(tree.toJSON()).toMatchSnapshot();
+});
+
+test("Completed Tab selected", () => {
+  const onSelect = jest.fn();
+  const tree = renderer.create(
+    <MenuTabs activeKey="Completed" onSelect={onSelect}></MenuTabs>
   );
   expect(onSelect).not.toHaveBeenCalled();
   expect(tree.toJSON()).toMatchSnapshot();
